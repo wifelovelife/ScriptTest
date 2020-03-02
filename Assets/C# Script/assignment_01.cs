c0e4c53399719bb5407d937b3bdf9efbe2633df3
tomohirokusazaki <to.co9pit@gmail.com>
2020-03-02 09:15:20 +0900

    [Add]ScriptTestプロジェクト　初めてのコミット
    Lesson_04課題提出です。Scenes[Assignment]にて制作しております。

---
+++ Assets/C# Script/assignment_01.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assignment_01 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] array = { 5, 60, 90, 22, 100 };
+
+        for (int i = 0; i < 5; i++) {
+            Debug.Log(array[i]);
+        }
+
+        for (int r = 4; r >= 0; r--) {
+            Debug.Log(array[r]);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/C# Script/assignment_02.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {
+    private int mp = 53;
+    public void Magic(int consume) {
+        int rest = this.mp -= consume;
+        if (rest >= 0) {
+            Debug.Log("魔法攻撃をした。残りMPは" + rest + "。");
+        } else {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    } 
+}
+
+public class assignment_02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Boss boss01 = new Boss();
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+        boss01.Magic(5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
